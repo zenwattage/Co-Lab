884208f851e49f8030f82e8b98be6a4b62d2bf8f
Jordynn Otto <j_taylor92@yahoo.com>
2019-07-17 20:15:00 -0700

    welcome page

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import "./style.css"; 
+import "./style.css";
 
 export default class Home extends Component {
 
@@ -8,11 +8,17 @@ export default class Home extends Component {
     // JSX
     return (
       <div>
-        <article class="container">
+        <h1 className="logo">CoLab Logo - THIS WILL BE AN IMAGE</h1>
+        <article className="container">
           <blockquote>
-            <strong>Welcome</strong> to <em>our creative</em>  <strong>community</strong>
+            <strong>Welcome</strong> to <em>our creative</em>  <strong className="community">community</strong>
           </blockquote>
         </article>
+
+        <div className="buttons">
+          <button type="button" className="login">Login</button>
+          <button type="button" className="login">Register</button>
+        </div>
       </div>
     );
   }
