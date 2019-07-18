5d900beed403fd8a4fd5760d63cb9f5a2535506f
Scott H <zenwattage@gmail.com>
2019-07-18 15:21:08 -0700

    login.jsx authentication and session added

--- client/src/page/Login.jsx
+++ client/src/page/Login.jsx
@@ -1,56 +1,65 @@
-import React, { Component } from 'react';
-import { } from 'react-router-dom'
-import axios from 'axios';
-import "./style.css"; 
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import axios from "axios";
+import "./style.css";
 
 export default class Login extends Component {
-
   state = {
     email: "",
     password: "",
     errorMessage: ""
-  }
+  };
 
   handleSubmit = event => {
     event.preventDefault();
     const { email, password } = this.state;
-    axios.post({
-      url: "/authentication/signin",
-      method: "POST",
-      data: {
-        email,
-        password
-      }
-    })
-    .then((response) => {
-      this.props.history.push('/profile');
-    })
-    .catch((error) => {
-      this.setState({
-        errorMessage: error.response.data.message
+    axios
+      .post({
+        url: "/authentication/signin",
+        method: "POST",
+        data: {
+          email,
+          password
+        }
+      })
+      .then(response => {
+        //set user
+        const isAuthenticated = response.data.isAuthenticated;
+        window.localStorage.setItem("isAuthenticated", isAuthenticated);
+        this.props.history.push("/profile");
+      })
+      .catch(error => {
+        this.setState({
+          errorMessage: error.response.data.message
+        });
       });
-    });
   };
 
-  handleChange = (event) => {
-    const {name, value} = event.target;
+  handleChange = event => {
+    const { name, value } = event.target;
     this.setState({
       [name]: value
-    })
+    });
   };
-    render() {
-      // JSX
-      return (
-          <div className="loginpage">
-            <h1 className="logintitle"> Log In</h1>
-            <form onSubmit={this.handleSubmit}>
-              <input type="text" name="email" onChange={this.handleChange} />
-              <input type="password" name="password" onChange={this.handleChange} />
-              <br></br>
-              <button className="login">Log In</button>
-            </form>
-            <p>{this.state.errorMessage}</p>
-          </div>
-      );
+  render() {
+    const isAuthenticated = window.localStorage.getItem("isAuthenticated");
+
+    if (isAuthenticated) {
+      return <Redirect to="/profile" />;
     }
+
+    // JSX
+    return (
+      <div className="loginpage">
+        <h1 className="logintitle"> Log In</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="email" onChange={this.handleChange} />
+          <input type="password" name="password" onChange={this.handleChange} />
+          <br />
+          <button className="login">Log In</button>
+        </form>
+        <p>{this.state.errorMessage}</p>
+      </div>
+    );
+  }
 }
